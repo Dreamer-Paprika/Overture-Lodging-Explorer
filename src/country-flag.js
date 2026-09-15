cb4c1c77c6032c22c5762b20817770470967ba53
Oseyijotham <vanclise277@gmail.com>
2026-09-15 23:17:44 +0100

    Update country-flag.js

--- src/country-flag.js
+++ src/country-flag.js
@@ -2,7 +2,7 @@ export function findFlag(country) {
   return fetch(`https://api.api-ninjas.com/v1/countryflag?country=${country}`, {
     method: 'GET',
     headers: {
-      'x-api-key':'TRIljCXEzfX6rr3l4Dr5KdZQneLlbZhaEALypASi',
+      'x-api-key': 'gdvQqEx3KBpR51ZfOeJgIA==10aHi9pat7JXS0OQ',
     },
   });
 }
