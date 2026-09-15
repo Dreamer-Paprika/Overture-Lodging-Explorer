ed97507b1ee52f7859294f7b00099a81b8cbac45
Oseyijotham <vanclise277@gmail.com>
2026-09-15 07:17:21 +0100

    Add

--- src/country-flag.js
+++ src/country-flag.js
@@ -1,12 +1,8 @@
 export function findFlag(country) {
-  return fetch(
-    `https://api.api-ninjas.com/v1/countryflag?country=${country}`,
-    {
-      method: 'GET',
-      headers: {
-        'X-Api-Key':
-          'gdvQqEx3KBpR51ZfOeJgIA==10aHi9pat7JXS0OQ',
-      },
-    }
-  );
+  return fetch(`https://api.api-ninjas.com/v1/countryflag?country=${country}`, {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': 'TRIljCXEzfX6rr3l4Dr5KdZQneLlbZhaEALypASi',
+    },
+  });
 }
